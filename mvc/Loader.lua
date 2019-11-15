4af61e0c54aa55fabbd625002bb28ec9218da272
LensarZhang <LensarZhang@126.com>
2019-11-15 19:25:24 +0800

    ﻿fix: 添加模块加载代码框架
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

---
+++ mvc/Loader.lua
@@ -0,0 +1,7 @@
+--[[--模块加载器
+@author LensarZhang
+
+Date   2019-11-15 19:20:39
+Last Modified by   LensarZhang
+Last Modified time 2019-11-15 19:22:12
+]]
---
+++ mvc/ModuleConfig.lua
@@ -0,0 +1,11 @@
+--[[--ldoc desc
+@author LensarZhang
+
+Date   2019-11-15 19:22:35
+Last Modified by   LensarZhang
+Last Modified time 2019-11-15 19:24:42
+]]
+local ModuleConfig = {}
+
+
+return ModuleConfig
