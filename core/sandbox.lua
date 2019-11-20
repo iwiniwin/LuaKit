ffb428017468e7b3f231208cc346ac48cc8566f5
iwiniwin <iwiniwin@foxmail.com>
2019-11-20 23:15:20 +0800

    删除冗余文件

--- core/sandbox.lua
+++ core/sandbox.lua
@@ -29,4 +29,22 @@ local f = {print = print, setfenv = setfenv}
 setfenv(1, f)
 print(x)
 
+---保护环境 人人有责
+local env = getfenv();
+local protectEnv = function(env)
+    local mt  = getmetatable(env);
+    local cache = {};
+    mt.__newindex = function( t,k,v )
+        if cache[k] == nil then
+            cache[k] = v;
+        else
+            if cache[k] ~= v then
+                error("不允许复写" .. k)
+            end
+        end
+        rawset(mt.__index, k, v)
+    end
+end
+protectEnv(env);
+
 -- http://timothyqiu.com/archives/lua-note-sandboxes/
